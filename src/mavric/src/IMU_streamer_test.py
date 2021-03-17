8f87fc3b1f99d6037b687348a2763ff1ba25142a
kaharder <78929773+kaharder@users.noreply.github.com>
2021-03-17 12:45:22 -0500

    Add basic functionality to publish x,y,z

--- src/mavric/src/IMU_streamer_test.py
+++ src/mavric/src/IMU_streamer_test.py
@@ -1,18 +1,23 @@
 import rospy
 import adafruit_bno055
-from std_msgs.msg import 
+from std_msgs.msg import double
 
 from busio import I2C
 from board import SDA, SCL
 
-imui2c = I2C(SCL, SDA)
+imui2c = busio.I2C(board.SCL, board.SDA)
 
 imu = adafruit_bno055.BNO055_I2C(imui2c)
 
-def t():
-        pub = rospy.Publisher('IMU_Data', ,queue_size = 10)
-    
-
+def imuoutput():
+        imudat = rospy.Publisher('IMU_Data', double, queue_size = 10)
+        rospy.init_node('IMU_Data')
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+                x, y, z = imu.euler
+                imudat.publish(x,y,z)
+                rate.sleep()
+               
 
 if __name__ == '__main__':
-        talker()
+        imuoutput()
